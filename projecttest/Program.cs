33bed76e08d2dd8238c130cf1c4fefe698324639
ZahwaLadyaMaharani <zahwa.ladya.ft20@mail.umy.ac.id>
2023-11-16 21:27:53 +0700

    Add project files.

---
+++ projecttest/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Menghitung Luas Segitiga");
+
+        // Input panjang alas
+        Console.Write("Masukkan panjang alas: ");
+        double alas = Convert.ToDouble(Console.ReadLine());
+
+        // Input tinggi segitiga
+        Console.Write("Masukkan tinggi segitiga: ");
+        double tinggi = Convert.ToDouble(Console.ReadLine());
+
+        // Hitung dan tampilkan luas segitiga
+        double luas = HitungLuasSegitiga(alas, tinggi);
+        Console.WriteLine($"Luas segitiga adalah: {luas}");
+
+        // Tunggu agar pengguna dapat melihat hasilnya
+        Console.ReadLine();
+    }
+
+    public static double HitungLuasSegitiga(double alas, double tinggi)
+    {
+        return 0.5 * alas * tinggi;
+    }
+}
---
+++ projecttestTests/ProgramTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using Assert = NUnit.Framework.Assert;
+
+public class ProgramTests
+{
+    [Test]
+    public void HitungLuasSegitiga_CorrectInput_ReturnsCorrectLuas()
+    {
+        // Arrange
+        double alas = 5;
+        double tinggi = 8;
+
+        // Act
+        double result = Program.HitungLuasSegitiga(alas, tinggi);
+
+        // Assert
+        Assert.AreEqual(20, result);
+    }
+
+    [Test]
+    public void HitungLuasSegitiga_NegativeInput_ThrowsException()
+    {
+        // Arrange
+        double alas = -5;
+        double tinggi = 8;
+
+        // Act & Assert
+        Assert.Throws<ArgumentException>(() => Program.HitungLuasSegitiga(alas, tinggi));
+    }
+}
